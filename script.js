ca73993b8da03c3ee4a4edc857f351e05498b355
Bora-Di <mbou7841@gmail.com>
2024-07-26 20:19:02 +0100

    commit

--- script.js
+++ script.js
@@ -1,25 +1,16 @@
-const jokeContainer = document.getElementById("joke");
+const jokeContainer =  document.getElementById("joke");
 const btn = document.getElementById("btn");
 const url = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,explicit&type=single";
 
-let getJoke = () => {
+let getJoke = () =>{
     jokeContainer.classList.remove("fade");
     fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);  // Log the entire response to check its structure
-        if (data && data.joke) {
-            jokeContainer.innerText = `${data.joke}`;
-        } else {
-            jokeContainer.innerText = "No joke found!";
-        }
-        jokeContainer.classList.add("fade");
-    })
-    .catch(error => {
-        console.error("Error fetching joke:", error);
-        jokeContainer.innerText = "Failed to fetch a joke.";
-    });
+    .then(data => data.json())
+    .then(item =>{
+        jokeContainer.textContent= `${item.joke}`;
+        jokeContainer.classList.add("fade"); 
+});
 }
 
-btn.addEventListener("click", getJoke);
+btn.addEventListener("click",getJoke);
 getJoke();
